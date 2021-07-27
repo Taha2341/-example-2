5102c13ca4cf3b78a1cba75e5772686a35884af4
Taha2341 <86522804+Taha2341@users.noreply.github.com>
2021-07-27 17:19:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ödev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string krediler =["hızlı kredi" ,"maaşını halkbankdan lanlara özel" ,"mutlu emekliihtiyaç kredisi "];
+            Console.WriteLine(krediler);
+        }
+    }
+}
